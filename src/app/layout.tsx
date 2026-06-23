165b3e489783efc5b3b80c1d41bac4d190896d51
Peter <peterfoxx93@gmail.com>
2026-06-23 07:41:58 -0400

    Add favicon (logo-clinica.png) to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"], display: "swap", variable: "--font-int
 export const metadata: Metadata = {
   title: "Centro Odontológico Dra. Reyna Pimentel | Expertos en crear sonrisas",
   description: "Centro Odontológico en Nagua, Rep. Dom. Especialistas en Diseño de Sonrisa, Ortodoncia, Endodoncia y más. Reconocidos por el SNS como mejor odontóloga zona Nordeste. ¡Agenda tu cita!",
+  icons: {
+    icon: "/images/logo-clinica.png",
+  },
 };
 
 export default function RootLayout({
